b95391cab2ecafef9a2118e13c10f437feb8d80b
E-Holt <emma.d.winch@gmail.com>
2022-07-12 19:27:42 +1000

    Changed dictionaries to lists, added some code to the hints function

---
+++ colourlist.py
@@ -0,0 +1,63 @@
+red_list = [
+    "This colour is associated with the Ruby logo",
+    "This colour is associated with the Angular logo",
+    "This colour is associated with the icon on the PyTorch logo",
+    "This colour is associated with the Ruby on Rails logo",
+    "This colour is associated with the Delphi logo"
+]
+
+orange_list = [
+    "This colour is associated with the “steam” on the current Java logo",
+    "This colour is associated with the HTML logo",
+    "With colour is associated with icon on the the Swift logo",
+    "This is one of the colours in the Rust logo that is not grey",
+    "This colour is associated with the icon on the TensorFlow logo"
+]
+
+yellow_list = [
+    "This colour is associated with the JavaScript logo and is not black",
+    "This is the colour of the bottom-right snake in the Python icon",
+    "This is the colour of the elephant in the Hadoop logo",
+    "This is the colour of Tux's feet in the Linux mascot",
+    "This is one the colour of the bottom-right square in the Microsoft icon"
+]
+
+green_list = [
+    "This is the colour of the hexagon in the Node icon",
+    "This colour is associated with the Django logo that is not white",
+    "This colour is associated with the Spring logo and is not white",
+    "This is one the colour of the top-right square in the Microsoft icon",
+    "This colour is associated with the Bosque logo and is not white"
+]
+
+blue_list = [
+    "This is the colour of the top-left snake in the Python icon",
+    "This colour is associated with the CSS icon",
+    "This colour is associated with the C++ icon",
+    "This colour is associated with the TypeScript icon",
+    "This is the colour associated with the “cup” in the Java icon"
+]
+
+purple_list = [
+    "This colour is associated with the C# icon",
+    "This colour is associated with the PHP logo and is not black or white",
+    "This colour is associated with the Elixir logo and is not black",
+    "This colour is associated with the Bootstrap logo and is not white",
+    "This colour is associated with the .NET logo and is not white"
+]
+
+black_list = [
+    "This colour is associated with the Hack logo and is not orange, white, or grey",
+    "This colour is associated with the Node logo and is not green",
+    "This colour is associated with the PHP logo and is not purple or white",
+    "This colour is associated with the Kotlin logo and is not purple or pink",
+    "This is the colour of Tux's body in the Linux mascot."
+]
+
+grey_list = [
+    "This is the colour of the oval in the R icon",
+    "This colour is associated with the Cordova logo and is not white or blue",
+    "This colour is associated with the Bash logo and is not white or green",
+    "This colour is associated with the C logo and is not white",
+    "This is one of the colours in the Rust logo and is not orange"
+]
--- guess.py
+++ guess.py
@@ -14,16 +14,47 @@
 #yes, continue game adding additional points. 
 #no, print("Thank you for playing!"), back to menu
 import random
+from colourlist import red_list, orange_list, yellow_list, green_list, blue_list, purple_list, black_list, grey_list
 
 colours = ["red", "yellow", "green", "blue", "purple", "black", "grey"]
 colour_pick = (random.choice(colours))
 
+def hints_choice():
+    if colour_pick == "red":
+        hints = red_list
+    if colour_pick == "orange":
+        hints = orange_list
+    if colour_pick == "yellow":
+        hints = yellow_list
+    if colour_pick == "green":
+        hints = green_list
+    if colour_pick == "blue":
+        hints = blue_list
+    if colour_pick == "purple":
+        hints = purple_list
+    if colour_pick == "black":
+        hints = black_list
+    if colour_pick == "grey":
+        hints = grey_list
+    print(hints)
+
+
+def giving_hints():
+    colour_hints = (random.choice(black_list))
+    print (colour_hints)
+
+
+
+
+
+
+
 #take colour_pick, choose hints
+#choose random hint from dicts
 #give hint for colour_pic
 #prompt guess
 #if guess correct, points
-#if guess incorrect -  hint/prompt
+#if guess incorrect -  
+#Give next random hint, not giving ones given
+#continue until 5 guesses
 #if guess not in list - error
-
-def hints():
-    pass
